0feb90fca7bf2bcd99344be97302e05126938946
ginchanah <152182564+ginchanah@users.noreply.github.com>
2024-11-30 22:26:29 +0100

    Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("hi");
+
+
+let menuButton = document.querySelector("header section button")
+let menuList = document.querySelector("header ul")
+
+menuButton.onclick = showMenu;
+
+function showMenu(){
+    menuList.classList.toggle("show")
+    console.log("Menu button clicked")  
+}
